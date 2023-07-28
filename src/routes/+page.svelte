fd715ff85cac72c0aa9a24a253c930ecf243166b
Alexander Richterich <alexander@richterich.dev>
2023-07-28 22:49:08 +0300

    Add page title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <h1 class="text-3xl font-bold underline">Hello, World!</h1>
 
+<svelte:head>
+	<title>Custom Inputs</title>
+</svelte:head>
+
 <style lang="postcss">
 	:global(html) {
 		background-color: theme(colors.gray.100);

037fd18ea0bfa04d38460098beaa5438e1e49e23
Alexander Richterich <alexander@richterich.dev>
2023-08-03 17:16:54 +0300

    Enhance base layout with header, footer and navbar

---
+++ src/lib/components/MobileNav.svelte
@@ -0,0 +1,102 @@
+<script>
+	import classes from '$lib/utilities/transitions';
+
+	export let closeHandler = () => {};
+</script>
+
+<div>
+	<div
+		class="fixed inset-0 z-50 overflow-hidden lg:hidden"
+		role="dialog"
+		aria-modal="true"
+		aria-labelledby="dialog-title"
+	>
+		<div
+			class="absolute inset-0 bg-slate-900/25 backdrop-blur-sm transition-opacity"
+			in:classes={{
+				duration: 150,
+				base: 'duration-150',
+				from: 'opacity-0',
+				to: 'opacity-100'
+			}}
+			out:classes={{
+				duration: 75,
+				base: 'duration-75',
+				from: 'opacity-100',
+				to: 'opacity-0'
+			}}
+		/>
+		<div
+			class="fixed inset-0 flex items-start justify-end overflow-y-auto"
+			in:classes={{
+				duration: 200,
+				base: 'duration-200',
+				from: 'translate-x-[min(20rem,calc(100vw-theme(spacing.10)))]',
+				to: 'translate-x-0'
+			}}
+			out:classes={{
+				duration: 200,
+				base: 'duration-200',
+				from: 'translate-x-0',
+				to: 'translate-x-[min(20rem,calc(100vw-theme(spacing.10)))]'
+			}}
+		>
+			<div
+				class="min-h-full w-[min(20rem,calc(100vw-theme(spacing.10)))] bg-white shadow-2xl ring-1 ring-black/10 transition"
+			>
+				<h2 class="sr-only" id="dialog-title">Navigation</h2>
+				<button
+					type="button"
+					class="absolute right-6 top-5 flex h-8 w-8 items-center justify-center"
+					tabindex="0"
+					on:click={closeHandler}
+					><span class="sr-only">Close navigation</span><svg
+						class="h-3.5 w-3.5 overflow-visible stroke-slate-900"
+						fill="none"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						xmlns="http://www.w3.org/2000/svg"
+						><path d="M0 0L14 14M14 0L0 14" />
+					</svg>
+				</button>
+				<nav class="divide-y divide-slate-900/10 text-base leading-7 text-slate-900">
+					<div class="px-8 py-6">
+						<a
+							class="flex-none select-none text-xl font-extrabold uppercase text-slate-900"
+							href="/"
+							on:click={closeHandler}
+						>
+							UI COMPONENTS
+						</a>
+					</div>
+					<div class="px-8 py-6">
+						<div class="-my-2 items-start space-y-2">
+							<a class="block w-full py-2 font-semibold" href="/" on:click={closeHandler}>
+								Components
+							</a>
+						</div>
+					</div>
+					<div class="px-8 py-6">
+						<div class="-my-2 space-y-4">
+							<a
+								class="block w-full py-2 font-semibold"
+								href="//github.com/richterich/ui-components/"
+								target="_blank"
+								on:click={closeHandler}
+							>
+								Browse repo
+							</a>
+							<a
+								class="inline-flex w-full justify-center rounded-lg bg-slate-900 px-4 py-3 text-sm font-semibold text-white hover:bg-slate-700"
+								href="/contact"
+								on:click={closeHandler}
+							>
+								<span>Contact <span aria-hidden="true">→</span></span>
+							</a>
+						</div>
+					</div>
+				</nav>
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,72 @@
 <script>
+	import MobileNav from '$lib/components/MobileNav.svelte';
 	import '../app.postcss';
+
+	let open = false;
+
+	const openNavigation = () => (open = true);
+	const closeNavigation = () => (open = false);
 </script>
 
-<header>
-	<p />
+<header class="relative z-50 w-full flex-none text-sm font-semibold leading-6 text-slate-900">
+	<nav class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+		<div class="relative flex items-center py-[2.125rem]">
+			<div class="absolute inset-x-0 bottom-0 h-px bg-slate-900/5" />
+			<a href="/" class="flex-none select-none text-xl font-extrabold uppercase text-slate-900">
+				ui components
+			</a>
+			<div class="ml-auto hidden lg:flex lg:items-center">
+				<a href="/">Components</a>
+			</div>
+			<button
+				type="button"
+				class="-my-1 -mr-1 ml-auto flex h-8 w-8 items-center justify-center lg:hidden"
+				on:click={openNavigation}
+				><span class="sr-only">Open navigation</span><svg
+					viewBox="0 0 24 24"
+					class="h-6 w-6 stroke-slate-900"
+					><path
+						d="M3.75 12h16.5M3.75 6.75h16.5M3.75 17.25h16.5"
+						fill="none"
+						stroke-width="1.5"
+						stroke-linecap="round"
+					/></svg
+				>
+			</button>
+			<div
+				class="lg:border-slate-900/15 hidden lg:ml-8 lg:flex lg:items-center lg:border-l lg:pl-8"
+			>
+				<a href="//github.com/richterich/ui-components/" target="_blank">Browse repo</a>
+				<a
+					class="-my-2.5 ml-8 inline-flex justify-center rounded-lg bg-slate-900 px-4 py-2.5 text-sm text-white hover:bg-slate-700"
+					href="/contact"
+				>
+					<span>Contact <span aria-hidden="true">→</span></span>
+				</a>
+			</div>
+		</div>
+	</nav>
 </header>
 
 <main class="relative mx-auto mt-20 w-full max-w-7xl px-4 sm:px-6 lg:px-8">
 	<slot />
 </main>
 
-<footer>
-	<p />
+<footer class="mx-auto mt-32 w-full max-w-7xl px-4 sm:px-6 lg:px-8">
+	<div class="border-t border-slate-900/5 py-10">
+		<p class="mt-5 text-center text-sm leading-6 text-slate-500">
+			Made with Svelte and Tailwind CSS
+		</p>
+	</div>
 </footer>
+
+{#if open}
+	<MobileNav closeHandler={closeNavigation} />
+{/if}
 
 <style lang="postcss">
 	:global(html) {
 		background-color: theme(colors.white);
+		font-family: 'Inter', system-ui, sans-serif;
 	}
 </style>

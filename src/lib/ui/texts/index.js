327bf7f511210e9d5aa7a1aa2f3406fc6955018c
Alexander Richterich <alexander@richterich.dev>
2023-08-17 22:14:57 +0300

    add policy text component

--- src/lib/ui/texts/index.js
+++ src/lib/ui/texts/index.js
@@ -3,11 +3,13 @@ import XsParagraphText from './xs-paragraph-text.svelte';
 import XsNestedParagraphText from './xs-nested-paragraph-text.svelte';
 import XlParagraphText from './xl-paragraph-text.svelte';
 import Xl3ParagraphText from './3xl-paragraph-text.svelte';
+import SmPolicyAgreeText from './sm-policy-agree-text.svelte';
 
 export {
 	XsParagraphText,
 	SmPragraphText,
 	XsNestedParagraphText,
 	XlParagraphText,
-	Xl3ParagraphText
+	Xl3ParagraphText,
+	SmPolicyAgreeText
 };
---
+++ src/lib/ui/texts/sm-policy-agree-text.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { merge } from '$lib/utilities/merge-classes';
+
+	/** @type {string} */
+	export let href = '';
+	/** @type {string} */
+	export let agreeText = '';
+	/** @type {string} */
+	export let policyName = '';
+	/** @type {{text:string, policy:string}} */
+	export let className = { text: '', policy: '' };
+</script>
+
+<p class={merge('text-sm leading-6 text-gray-500', className.text)}>
+	{agreeText}
+	<a
+		{href}
+		class={merge('font-semibold leading-6 text-indigo-600 hover:text-indigo-500', className.policy)}
+	>
+		{policyName}
+	</a>.
+</p>

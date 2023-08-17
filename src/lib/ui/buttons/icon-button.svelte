66cab84a13f1712fc955bdd7875e41faeaa610c1
Alexander Richterich <alexander@richterich.dev>
2023-08-17 22:15:25 +0300

    add button components

---
+++ src/lib/ui/buttons/icon-button.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { merge } from '$lib/utilities/merge-classes';
+
+	/** @type {string} */
+	export let text = '';
+	/** @type {string} */
+	export let className = '';
+	/** @type {() => any} */
+	export let click = () => {};
+</script>
+
+<button
+	type="button"
+	class={merge('flex h-8 w-8 items-center justify-center', className)}
+	on:click={click}
+>
+	<span class="sr-only">{text}</span>
+	<slot name="icon" />
+</button>
---
+++ src/lib/ui/buttons/submit-button.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { merge } from '$lib/utilities/merge-classes';
+
+	/** @type {string} */
+	export let text = '';
+	/** @type {string} */
+	export let className = '';
+	/** @type {() => any} */
+	export let click = () => {};
+</script>
+
+<button
+	type="submit"
+	class={merge(
+		'block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
+		className
+	)}
+	on:click={click}
+>
+	{text}
+</button>
